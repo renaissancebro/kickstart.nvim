1399b0dfa233ed5e464656e010b9a0b375edc165
josh <jcfreeman23@gmail.com>
2025-07-19 03:35:31 -0500

    Claude: Modified options.lua (2025-07-19_03-35-31)

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,42 +1,16 @@
 vim.opt.clipboard = 'unnamedplus'
 
--- Debug command to see treesitter groups
-vim.api.nvim_create_user_command('TSHighlight', function()
-  local ts_utils = require('nvim-treesitter.ts_utils')
-  local node = ts_utils.get_node_at_cursor()
-  if node then
-    print("Node type: " .. node:type())
-    local hl_group = vim.treesitter.get_captures_at_cursor(0)
-    for _, capture in ipairs(hl_group) do
-      print("Highlight group: @" .. capture)
-    end
-  end
-end, {})
-
--- Enhanced f-string highlighting - trying all possible groups
-vim.api.nvim_create_autocmd({'ColorScheme', 'VimEnter'}, {
+-- F-string bracket highlighting
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'python',
   callback = function()
-    -- Try multiple possible f-string highlight groups
-    local fstring_highlights = {
-      '@string.special',
-      '@string.interpolation', 
-      '@string.escape',
-      '@punctuation.delimiter',
-      '@punctuation.bracket',
-      '@embedded',
-      '@variable.builtin',
-      '@keyword.operator',
-      '@operator',
-      -- Language specific
-      '@string.special.python',
-      '@string.interpolation.python',
-      '@punctuation.delimiter.python',
-      '@punctuation.bracket.python',
-      '@embedded.python',
-    }
+    -- Define highlight group for f-string brackets
+    vim.api.nvim_set_hl(0, 'FStringBracket', { fg = '#ffaa00', bold = true })
     
-    for _, group in ipairs(fstring_highlights) do
-      vim.api.nvim_set_hl(0, group, { fg = '#ffcc00', bold = true })
-    end
+    -- Use vim syntax to match brackets inside f-strings
+    vim.cmd([[
+      syntax clear pythonStrFormat
+      syntax match FStringBracket /{\|}/
+    ]])
   end,
 })
