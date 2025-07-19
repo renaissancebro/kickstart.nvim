f98c307d83650c4c1afcddb3aeca5a2a1a3d6b35
josh <jcfreeman23@gmail.com>
2025-07-19 03:41:34 -0500

    Claude: Modified options.lua (2025-07-19_03-41-34)

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,7 +1,7 @@
 vim.opt.clipboard = 'unnamedplus'
 
 -- F-string bracket highlighting
-vim.api.nvim_create_autocmd({'ColorScheme', 'VimEnter'}, {
+vim.api.nvim_create_autocmd({'ColorScheme', 'VimEnter', 'FileType'}, {
   callback = function()
     -- Define the highlight group for f-string brackets
     vim.api.nvim_set_hl(0, '@punctuation.bracket.fstring', { 
@@ -9,5 +9,20 @@ vim.api.nvim_create_autocmd({'ColorScheme', 'VimEnter'}, {
       bold = true,
       bg = '#2a2a2a'
     })
+    
+    -- Force treesitter to reload queries
+    pcall(function()
+      vim.treesitter.query.invalidate_cache()
+    end)
   end,
 })
+
+-- Debug command to test highlighting
+vim.api.nvim_create_user_command('TestFString', function()
+  vim.api.nvim_set_hl(0, '@punctuation.bracket.fstring', { 
+    fg = '#ff0000', 
+    bold = true,
+    bg = '#ffff00'
+  })
+  print("F-string highlight set to red on yellow")
+end, {})
