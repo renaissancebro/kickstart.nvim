1ecdc31babd40fedf6610f34115a431a6a742f65
josh <jcfreeman23@gmail.com>
2025-07-18 19:00:38 -0500

    Claude: Created test_completion.lua (2025-07-18_19-00-38)

---
+++ test_completion.lua
@@ -0,0 +1,10 @@
+-- Test file for completion
+local function test_function()
+  print("Hello world")
+  
+  -- Try typing vim. here and see if completion shows up
+  vim.
+end
+
+-- Try typing 'pr' and see if 'print' appears in completion
+pr
