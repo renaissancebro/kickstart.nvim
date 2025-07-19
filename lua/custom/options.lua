68fd45e46a4a83f415b3a6930ff35d7f35eebc08
josh <jcfreeman23@gmail.com>
2025-07-19 03:36:39 -0500

    Claude: Modified options.lua (2025-07-19_03-36-39)

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,16 +1,31 @@
 vim.opt.clipboard = 'unnamedplus'
 
--- F-string bracket highlighting
-vim.api.nvim_create_autocmd('FileType', {
+-- F-string bracket highlighting with higher priority
+vim.api.nvim_create_autocmd({'FileType', 'BufEnter', 'TextChanged', 'TextChangedI'}, {
   pattern = 'python',
   callback = function()
-    -- Define highlight group for f-string brackets
-    vim.api.nvim_set_hl(0, 'FStringBracket', { fg = '#ffaa00', bold = true })
+    -- Define highlight groups with high priority
+    vim.api.nvim_set_hl(0, 'FStringBracket', { fg = '#ff6600', bold = true })
+    vim.api.nvim_set_hl(0, 'FStringContent', { fg = '#00ff88', italic = true })
     
-    -- Use vim syntax to match brackets inside f-strings
-    vim.cmd([[
-      syntax clear pythonStrFormat
-      syntax match FStringBracket /{\|}/
-    ]])
+    -- Disable treesitter highlighting temporarily to test
+    pcall(function()
+      vim.cmd([[
+        syntax enable
+        syntax clear
+        syntax match FStringBracket /[{}]/ containedin=ALL
+        syntax region FStringContent start=/{/ end=/}/ containedin=ALL
+      ]])
+    end)
+  end,
+})
+
+-- Also try overriding treesitter highlights with higher priority
+vim.api.nvim_create_autocmd({'ColorScheme', 'VimEnter', 'BufEnter'}, {
+  pattern = '*',
+  callback = function()
+    -- Override with higher priority
+    vim.api.nvim_set_hl(0, '@punctuation.bracket', { fg = '#ff6600', bold = true })
+    vim.api.nvim_set_hl(0, '@punctuation.delimiter', { fg = '#ff6600', bold = true })
   end,
 })
