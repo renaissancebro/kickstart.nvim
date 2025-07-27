8bafc5764fdb665b9e25476128ba6863cd766a41
josh <jcfreeman23@gmail.com>
2025-07-27 15:18:58 -0500

    Claude: Created highlights.scm (2025-07-27_15-18-58)

---
+++ queries/css/highlights.scm
@@ -0,0 +1,12 @@
+; extends
+
+; CSS property names
+(property_name) @CSSProperty (#set! "priority" 110)
+
+; CSS selectors
+(class_selector) @CSSSelector (#set! "priority" 110)
+(id_selector) @CSSSelector (#set! "priority" 110)
+(tag_selector) @CSSSelector (#set! "priority" 110)
+
+; Enhanced bracket highlighting for CSS
+["{" "}" "[" "]" "(" ")"] @CustomBrackets (#set! "priority" 105)
