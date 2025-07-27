c5c1635a4daf39168a27a3315e637a57a259aad2
josh <jcfreeman23@gmail.com>
2025-07-27 15:16:07 -0500

    Claude: Modified highlights.scm (2025-07-27_15-16-07)

--- queries/python/highlights.scm
+++ queries/python/highlights.scm
@@ -1,4 +1,18 @@
 ; extends
 
-; Simple test - highlight ALL brackets in python files
-["{" "}"] @punctuation.bracket.fstring (#set! "priority" 105)
+; F-string highlighting
+(string
+  (string_start) @PythonFString
+  (string_content)? @PythonFString
+  (string_end) @PythonFString
+  (#match? @PythonFString "^f")
+  (#set! "priority" 110))
+
+; Enhanced bracket highlighting for all bracket types
+["(" ")" "[" "]" "{" "}"] @PythonBrackets (#set! "priority" 105)
+
+; F-string interpolation brackets (highest priority)
+(interpolation
+  "{" @PythonFString
+  "}" @PythonFString
+  (#set! "priority" 115))
