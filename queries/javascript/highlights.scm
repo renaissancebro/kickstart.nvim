bd4579d4ae937649044f79cd925e7236864f1e6b
josh <jcfreeman23@gmail.com>
2025-07-27 15:40:45 -0500

    Claude: Modified highlights.scm (2025-07-27_15-40-45)

--- queries/javascript/highlights.scm
+++ queries/javascript/highlights.scm
@@ -1,13 +1,12 @@
 ; extends
 
-; Template literals (backticks) highlighting
-(template_string) @JSTemplateLiteral (#set! "priority" 110)
-
-; Enhanced bracket highlighting
-["(" ")" "[" "]" "{" "}"] @CustomBrackets (#set! "priority" 105)
-
-; Template substitution brackets
+; VS Code-style template literal highlighting - only highlight substitution braces
+; Template substitution braces only (like VS Code)
 (template_substitution
-  "${" @JSTemplateLiteral
-  "}" @JSTemplateLiteral
+  "${" @JSTemplateBrace
+  "}" @JSTemplateBrace
   (#set! "priority" 115))
+
+; Enhanced bracket highlighting for normal brackets
+["(" ")" "[" "]"] @CustomBrackets (#set! "priority" 105)
+; Note: {} braces in template literals get special treatment above
