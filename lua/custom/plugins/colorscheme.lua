4e8b6664212ce1b2d9fb4dd13aaab9e04511b44b
josh <jcfreeman23@gmail.com>
2025-07-27 15:36:49 -0500

    Claude: Modified colorscheme.lua (2025-07-27_15-36-49)

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -24,14 +24,14 @@ return {
         colors = {}, -- Override default colors
         highlights = {
           -- Custom highlights for our syntax
-          PythonFString = { fg = '#e5c07b', fmt = 'bold' },
-          JSTemplateLiteral = { fg = '#e5c07b', fmt = 'bold' },
-          CustomBrackets = { fg = '#56b6c2' },
-          HTMLTag = { fg = '#c678dd', fmt = 'bold' },
-          HTMLAttr = { fg = '#98c379' },
-          CSSProperty = { fg = '#c678dd' },
-          CSSSelector = { fg = '#e5c07b', fmt = 'bold' },
-          Function = { fg = '#61afef', fmt = 'bold' },
+          PythonFString = { fg = '#e06c75', fmt = 'bold' },       -- Bright red for f-strings
+          JSTemplateLiteral = { fg = '#e06c75', fmt = 'bold' },   -- Bright red for template literals
+          CustomBrackets = { fg = '#56b6c2' },                   -- Cyan for brackets
+          HTMLTag = { fg = '#c678dd', fmt = 'bold' },            -- Purple for HTML tags
+          HTMLAttr = { fg = '#98c379' },                         -- Green for HTML attributes
+          CSSProperty = { fg = '#c678dd' },                      -- Purple for CSS properties
+          CSSSelector = { fg = '#e5c07b', fmt = 'bold' },        -- Yellow for CSS selectors
+          Function = { fg = '#61afef', fmt = 'bold' },           -- Blue for functions
         }
       }
       vim.cmd.colorscheme 'onedark'
